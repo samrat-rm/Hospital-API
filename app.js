ad4d77bdf9bf5c61b9d27aa641b3a53653b74f74
Samrat <smaratrm2@gmail.com>
2023-01-06 00:56:44 +0530

    Swagger Docs added

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 require("dotenv").config();
 require("express-async-errors"); // async wrapper
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
-const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocs = YAML.load("./swagger.yaml");
+
 const psychiatristRouter = require("./routes/psychiatrist");
 const patientRouter = require("./routes/patient");
 const hospitalRouter = require("./routes/hospital");
 const detailsRouter = require("./routes/allDetails");
+
+const cors = require("cors");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-    res.send("<h1>Hospital API</h1><a href='/api-docs'>Documentation</a>");
+    res.send(
+        "<h1>Hospital API</h1><a href='/api-docs'>Documentation</a><p>Users can find the psychiatrist and patient data for the hospital using the hospital ID in the 'details/:id' API.<br />And the hospital Id can be found in the 'get hospitals API' </p>"
+    );
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use("/api/v1/psychiatrists", psychiatristRouter);
 app.use("/api/v1/patients", patientRouter);
